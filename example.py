a79095f7e828963bb96def7fd9bf29e4954366f7
Masood Tariq <55462501+masoodtariq@users.noreply.github.com>
2020-08-10 23:24:53 +0500

    gnBFB

--- example.py
+++ example.py
@@ -2,5 +2,6 @@ def git_operation():
  print("i am editing  this file")
  print("my name is masood tariq")
  print("vrevE")
+ print("Expectance of a discrete random variable is obtained by multipyling the probability by x")
  print("i am creating my git directory")
 def git_operation()
